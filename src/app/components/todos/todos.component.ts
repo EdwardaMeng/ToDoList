00383872487b71fc271180d410c56847858bcfed
EdwardMeng <edwardameng@126.com>
2022-03-14 18:09:05 -0400

    Initial commit

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from './../models/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+
+  todos!: Todo[];
+
+  inputTodo = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.todos = [
+      {
+        content: 'First todo',
+        completed: false
+      },
+      {
+        content: 'Second todo',
+        completed: false
+      }
+    ];
+  }
+
+  // tslint:disable-next-line:typedef
+  toggleDone(id: number) {
+    this.todos.map((v, i) => {
+      // tslint:disable-next-line:triple-equals
+      if (i == id) { v.completed = !v.completed; }
+
+      return v;
+    });
+  }
+
+  // tslint:disable-next-line:typedef
+  deleteTodo(id: number) {
+    this.todos = this.todos.filter((v, i) => i !== id);
+  }
+
+  // tslint:disable-next-line:typedef
+  addTodo() {
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false
+    });
+
+    this.inputTodo = '';
+  }
+
+}
